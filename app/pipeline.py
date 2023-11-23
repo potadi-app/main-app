025553b35d7820500af074f7778077643367d315
eldivategar <tegareldiva@students.amikom.ac.id>
2023-11-23 13:54:37 +0700

    try remove signup 2

--- app/pipeline.py
+++ app/pipeline.py
@@ -1,17 +1,14 @@
 # di file yang berbeda, misalnya pipelines.py
 from allauth.socialaccount import app_settings
 from allauth.socialaccount.helpers import complete_social_login
-from allauth.socialaccount.models import SocialLogin, SocialAccount
-from allauth.account.models import EmailAddress
-from allauth.account.utils import perform_login
 from django.shortcuts import redirect
 from django.http import HttpResponse
+from django.dispatch import receiver
+from allauth.socialaccount.signals import pre_social_login
+from django.shortcuts import redirect
 
-def superuser_check(request, sociallogin):
-    # Pastikan pengguna bukan superuser
+@receiver(pre_social_login)
+def superuser_check(sender, request, sociallogin, **kwargs):
     if sociallogin.is_existing:
         user = sociallogin.user
-        if user.is_superuser:
-            return HttpResponse('halaman tidak diizinkan')
-
-    return complete_social_login(request, sociallogin)
+        return HttpResponse('halaman_tidak_diizinkan')
