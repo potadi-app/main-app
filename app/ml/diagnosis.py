a637f384fd9f820abc2a3245f65445c9cfcd82b0
eldivategar <tegareldiva@students.amikom.ac.id>
2024-03-01 15:56:14 +0800

    testing

--- app/ml/diagnosis.py
+++ app/ml/diagnosis.py
@@ -9,10 +9,10 @@ ALLOWED_EXTENSIONS = set(['jpg', 'png', 'jpeg', 'JPG', 'PNG', 'JPEG'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+model = load_model(f'{BASE_DIR}/app/ml/model/model_MobileNet_imagenet_New_224.h5')
+model.load_weights(f'{BASE_DIR}/app/ml/model/weights_MobileNet_imagenet_New_224.h5')
 
 def predict(file):
-    model = load_model(f'{BASE_DIR}/app/ml/model/model_MobileNet_imagenet_New_224.h5')
-    model.load_weights(f'{BASE_DIR}/app/ml/model/weights_MobileNet_imagenet_New_224.h5')
     try:
         img = Image.open(BytesIO(file)).convert("RGB")
         img = img.resize((224, 224))
