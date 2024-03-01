046bcc12be3883d1b9c83b85beca665630a8612f
eldivategar <tegareldiva@students.amikom.ac.id>
2024-03-01 18:15:57 +0800

    add cache in not_found error page

--- app/errors.py
+++ app/errors.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
+cache_page(60 * 60)
 def not_found(request, exception=None, requested_url=None):
     return render(request, 'errors/not_found.html')
