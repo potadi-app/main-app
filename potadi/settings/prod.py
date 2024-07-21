35aa7a70b0fb5abfb81223052d7054ec365c284a
eldivategar <tegareldiva@students.amikom.ac.id>
2024-07-21 22:31:58 +0700

    refact: prod settings

--- potadi/settings/prod.py
+++ potadi/settings/prod.py
@@ -1,4 +1,8 @@
 from .settings import *
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 DEBUG = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -6,8 +10,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/etc/mysql/my.cnf',
-        },
-    }
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    },
 }
